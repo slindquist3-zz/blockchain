1111177d50d1daa3a493f2bc1996f8fa1e0b20a3
scott <slindquist3@gmail.com>
2019-04-13 01:14:08 -0400

    initial styling

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -3,21 +3,39 @@ import styled from 'styled-components';
 
  const SearchBar = (props) => {
 
+    const InputContainer = styled.div`
+      height: 60px;
+
+    `
+
     const input = {
-      width: '400px',
+      width: '300px',
       height: '40px',
-      margin: '100px'
+      margin: '100px',
+      border: '2px solid #004a7c',
+      borderRadius: '15px'
+
     }
 
     const Button = styled.button`
-      width: 400px;
+      width: 300px;
       height: 40px;
-      margin: 100px;
+      margin: 50px;
+      background-color: #004a7c;
+      cursor: pointer;
+      border-radius: 15px;
+      color: white;
+      font-size: 18px;
     `
 
     return (
       <div className="SearchBar">
-          <input type="text" placeholder="Your Search Here" style={input} onChange={props.handleChange}/>
+          <div>
+          <InputContainer>
+            <img src={require('./search-icon.png')}/>
+            <input type="text" placeholder=" Your Search Here" style={input} onChange={props.handleChange}/>
+          </InputContainer>
+          </div>
           <Button onClick={props.handleSearch}>Search</Button>
       </div>
     )
